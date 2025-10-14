cfcd2f49ab0c52646ac982032ea10c8f0e30062e
Paul Ebert <paul.ebert@mail.de>
2025-10-14 15:11:59 +0200

    try again

--- pytpq/__init__.py
+++ pytpq/__init__.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-from .tpqdata import TPQData, find_all_tpq_files
-
-
-
-
-
+from pytpq.tpqdata import TPQData, find_all_tpq_files
 from pytpq.ensemble import Ensemble
 from pytpq.basic import tmatrix, get_shifts, ground_state_energy
 from pytpq.thermo import qn_moment_sum, moment_sum, operator_sum, partition, energy, entropy, specific_heat, thermodynamics, quantumnumber, susceptibility, operator
@@ -13,8 +8,3 @@ from pytpq.dos import dos_spectra
 from pytpq.statistics_for_tpq import mean, error, jackknife, error_jackknife
 from pytpq.utils import write_fwf
 from pytpq.linalg import tmatrix_e0, tmatrix_eigvals, tmatrix_eig, boltzmann_tensor, moment_average, operator_average
-
-__all__ = [
-    "TPQData", 
-    "find_all_tpq_files"
-    ]

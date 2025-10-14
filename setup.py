121f547075a6dfab11d5c6e4b46f035d7dba9bd2
Paul Ebert <paul.ebert@mail.de>
2025-10-14 10:21:21 +0200

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,14 @@ setup(
     name="pytpq",
     version="1.0",
     packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'numexpr',
+        'inspect',
+        'multiprocessing',
+        'functools',
+        'joblib',
+        'h5py',
+        ],
 )

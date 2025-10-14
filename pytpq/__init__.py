835156362f43c0c66e44708c8f67902e7f718a63
Paul Ebert <paul.ebert@mail.de>
2025-10-14 15:00:46 +0200

    trying to fix __init__.py to export some functions.

--- pytpq/__init__.py
+++ pytpq/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from pytpq.tpqdata import TPQData, find_all_tpq_files
+from .tpqdata import TPQData, find_all_tpq_files
+
+
+
+
+
 from pytpq.ensemble import Ensemble
 from pytpq.basic import tmatrix, get_shifts, ground_state_energy
 from pytpq.thermo import qn_moment_sum, moment_sum, operator_sum, partition, energy, entropy, specific_heat, thermodynamics, quantumnumber, susceptibility, operator
@@ -8,3 +13,8 @@ from pytpq.dos import dos_spectra
 from pytpq.statistics_for_tpq import mean, error, jackknife, error_jackknife
 from pytpq.utils import write_fwf
 from pytpq.linalg import tmatrix_e0, tmatrix_eigvals, tmatrix_eig, boltzmann_tensor, moment_average, operator_average
+
+__all__ = [
+    "TPQData", 
+    "find_all_tpq_files"
+    ]
